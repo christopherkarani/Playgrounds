a85f99800cc6b7db9ef49cbb205b67a0f24fde9d
xrisyz <chrisbkarani@gmail.com>
2018-04-26 11:20:16 +0300

    Finished Tree on Missing COdable values and Array of JSON situations

---
+++ 26:04:18.playground/Contents.swift
@@ -0,0 +1,196 @@
+import Foundation
+
+var str = "Hello, playground"
+
+extension Data {
+    var stringDescription: String {
+        return String(data: self, encoding: .utf8)!
+    }
+}
+
+
+print(str)
+
+let json = """
+{
+    "work": {
+        "id": 2422333,
+        "popularity": null,
+        "sponsor": "Random Publisher, Inc",
+        "books_count": 222,
+        "ratings_count": 860687,
+        "text_reviews_count": 37786,
+        "best_book": {
+            "id": 375802,
+            "title": "Ender's Game (Ender's Saga, #1)",
+            "author": {
+                "id": 589,
+                "name": "Orson Scott Card"
+            }
+        },
+     "candidates": [
+            {
+                "id": 44687,
+                "title": "Enchanters' End Game (The Belgariad, #5)",
+                "author": {
+                    "id": 8732,
+                    "name": "David Eddings"
+                }
+            },
+            {
+                "id": 22874150,
+                "title": "The End Game",
+                "author": {
+                    "id": 6876994,
+                    "name": "Kate  McCarthy"
+                }
+            },
+            {
+                "id": 7734468,
+                "title": "Ender's Game: War of Gifts",
+                "author": {
+                    "id": 236462,
+                    "name": "Jake Black"
+                }
+            }
+        ]
+    }
+}
+""".data(using: .utf8)!
+
+struct Author: Codable {
+    let id: Int
+    let name: String
+}
+
+struct Book: Codable {
+    let id: Int
+    let title: String
+    let author: Author
+}
+
+struct SearchResult: Decodable {
+    let id: Int
+    let booksCount: Int
+    let ratingsCount: Int
+    let textReviewsCount: Int
+    let bestBook: Book
+    let candidates: [Book]
+    
+    enum OuterKeys: String, CodingKey {
+        case work, candidates
+    }
+    
+    enum InnerKeys: String, CodingKey {
+        case id
+        case booksCount
+        case ratingsCount
+        case textReviewsCount
+        case bestBook
+        case candidates
+    }
+    
+    init (from decoder: Decoder) throws {
+        let outercontainer = try decoder.container(keyedBy: OuterKeys.self)
+        let innerContainer = try outercontainer.nestedContainer(keyedBy: InnerKeys.self, forKey: .work)
+        id = try! innerContainer.decode(Int.self, forKey: .id)
+        booksCount = try! innerContainer.decode(Int.self, forKey: .booksCount)
+        ratingsCount = try! innerContainer.decode(Int.self, forKey: .ratingsCount)
+        textReviewsCount = try! innerContainer.decode(Int.self, forKey: .textReviewsCount)
+        bestBook = try! innerContainer.decode(Book.self, forKey: .bestBook)
+        candidates = try! innerContainer.decode([Book].self, forKey: .candidates)
+    }
+}
+
+extension SearchResult: Encodable{
+    func encode(to encoder: Encoder) throws {
+        var outerContainer =  encoder.container(keyedBy: OuterKeys.self)
+        var innerContainer =  outerContainer.nestedContainer(keyedBy: InnerKeys.self, forKey: .work)
+        try innerContainer.encode(id, forKey: .id)
+        try innerContainer.encode(booksCount, forKey: .booksCount)
+        try innerContainer.encode(ratingsCount, forKey: .ratingsCount)
+        try innerContainer.encode(textReviewsCount, forKey: .textReviewsCount)
+        try innerContainer.encode(bestBook, forKey: .bestBook)
+        try innerContainer.encode(candidates, forKey: .candidates)
+    }
+}
+
+let decoder = JSONDecoder()
+decoder.keyDecodingStrategy = .convertFromSnakeCase
+
+let encoder = JSONEncoder()
+encoder.keyEncodingStrategy = .convertToSnakeCase
+
+let result = try! decoder.decode(SearchResult.self, from: json)
+let encodedResult = try! encoder.encode(result)
+
+print(encodedResult.stringDescription)
+print(result.booksCount)
+
+let jsonTwo = """
+{
+  "result": {
+    "title": "My First Blog Post",
+    "author": "Pasan Premaratne",
+    "date": "April 16, 2018"
+  }
+}
+""".data(using: .utf8)!
+
+struct Post: Codable {
+    let title: String
+    let author: String
+    let date: String
+}
+
+
+let post = try! decoder.decode([String:Post].self, from: jsonTwo)
+
+
+let jsonThree = """
+{
+  "results": [
+    {
+      "code": 200
+    },
+    {
+      "code": 401
+    },
+    {
+      "code": 320
+    }
+  ]
+}
+""".data(using: .utf8)!
+
+struct Status: Codable {
+    let code: Int
+}
+
+
+let statusStuff = try! decoder.decode([String : [Status]].self, from: jsonThree)
+statusStuff.count
+statusStuff.forEach { (key, value) in
+    value.forEach({ (status) in
+        print(status.code)
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
