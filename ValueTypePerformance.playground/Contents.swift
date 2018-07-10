5788df402c0e7ebc450194f0b523e532a7df6d7d
xrisyz <chrisbkarani@gmail.com>
2018-07-10 14:34:59 +0300

    update

--- ValueTypePerformance.playground/Contents.swift
+++ ValueTypePerformance.playground/Contents.swift
@@ -1,5 +1,19 @@
  //: Playground - noun: a place where people can play
 
-import UIKit
+import Foundation
 
 var str = "Hello, playground"
+
+ 
+ var sampleBytes : [UInt8] = [0x0b, 0xad, 0xf0, 0x0d]
+ 
+ 
+ 
+ var nsData = NSMutableData(bytes: sampleBytes, length: sampleBytes.count )
+let nsOtherData = nsData.mutableCopy() as! NSMutableData
+ nsData.append(sampleBytes, length: sampleBytes.count)
+ 
+ 
+var data = Data(bytes: sampleBytes)
+ 
+ 
