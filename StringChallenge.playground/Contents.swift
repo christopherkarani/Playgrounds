e52b19ce9961599859467b6fa49d87b1c1a1913f
xrisyz <chrisbkarani@gmail.com>
2018-07-19 13:03:31 +0300

    update

--- StringChallenge.playground/Contents.swift
+++ StringChallenge.playground/Contents.swift
@@ -4,13 +4,22 @@ import Foundation
 
 var str = "Hello, playground"
 
+extension Bool {
+    mutating func toggle()  {
+        self = !self
+    }
+}
+
+var gay : Bool = false
+gay.toggle()
+
 enum AlphaBet: String {
     case a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z
 }
 
 extension AlphaBet {
-    func returnLetter() -> String {
-        switch self {
+    static func returnLetter(input: AlphaBet) -> Character {
+        switch input {
         case .a:
             return "b"
         case .b:
@@ -18,7 +27,7 @@ extension AlphaBet {
         case .c:
             return "d"
         case .d:
-            return "e"
+            return "E"
         case .e:
             return "f"
         case .f:
@@ -26,7 +35,7 @@ extension AlphaBet {
         case .g:
             return "h"
         case .h:
-            return "i"
+            return "I"
         case .i:
             return "j"
         case .j:
@@ -41,23 +50,53 @@ extension AlphaBet {
             return "O"
         case .p:
             return "q"
-        default:
-            return "Z"
+        case .q:
+            return "r"
+        case .s:
+            return "s"
+        case .t:
+            return "U"
+        case .v:
+            return "w"
+        case .w:
+            return "x"
+        case .x:
+            return "y"
+        case .z:
+            return "a"
+        case .l:
+            return "m"
+        case .r:
+            return "s"
+        case .u:
+            return "v"
+        case .y:
+            return "z"
         }
     }
 }
 
-
-func letterChanges(_ str: String) {
-    for i in str {
-        print(i)
+extension AlphaBet {
+    static func all() -> [AlphaBet] {
+        return [.a,.b,.c,.d,.e,.f,.g,.h,.i,.j,.k,.l,.m,.n,.o,.p,.q,.r,.s,.t,.u,.v,.w,.x,.y,.z]
     }
 }
 
-//let alphabet = "A"..."Z"
+
+let startChar = Unicode.Scalar("A").value
+let endChar = Unicode.Scalar("Z").value
 
 
-
+for letter in str {
+    for alphabet in startChar...endChar {
+        if let char = Unicode.Scalar(alphabet) {
+            print(char)
+            if letter == Character(char) {
+                print(char)
+            }
+        }
+    }
+}
 
 
 //letterChanges(str)
